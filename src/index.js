b3b295c21670216c4208f4e54a4285ed939b62ac
Prince-hub716 <princegarg86844@gmail.com>
2024-12-31 14:32:06 +0530

    db connected

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path:'./env'});
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./env'}
+)
+
+
+connectDB()
+
+
+
+
+
+
+
+
+// import express from "express"
+
+// const app=express()
+
+
+
+// ;(async ()=>{
+//     try{
+//  await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//  app.on("error",(err)=>{
+//     console.log("Error",err);
+//     throw err
+//  })
+
+//  app.listen(process.env.PORT,()=>{
+//     console.log(`App is listening at port ${process.env.PORT}`)
+//  })
+//     }
+//     catch(err){
+//         console.log("Error",err)
+//         throw err
+//     }
+// })()
